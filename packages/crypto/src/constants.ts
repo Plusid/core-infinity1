3180f5168254bd6147aee17dc720e0af94e8c41d
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-05-03 16:23:06 -0600

    Update constants.ts

--- packages/crypto/src/constants.ts
+++ packages/crypto/src/constants.ts
@@ -1,4 +1,4 @@
-export const SATOSHI: number = 10000000;
+export const SATOSHI: number = 1000000;
 // 1e8 to 100000000 is working
-// next try with 10000000
+// second try with 1000000
 export const ARKTOSHI: number = SATOSHI;
